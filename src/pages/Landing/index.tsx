4918a3fa680200af5ba18b81b99dc7c79406bbf7
Victor <victormarques.ia@gmail.com>
2020-08-05 15:25:11 -0300

    Update Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -10,6 +11,14 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 export default function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then((response) => {
+      setTotalConnections(response.data.total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -37,7 +46,7 @@ export default function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
