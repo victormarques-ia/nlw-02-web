f4229dbfe779854e0bcdd8ea4e1be7dd4195de40
Victor Silva <victormarques.ia@gmail.com>
2020-08-09 10:26:48 -0300

    Create global styles with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import GlobalStyles from './assets/styles/GlobalStyles';
+// import './assets/styles/global.css'
 
-import './assets/styles/global.css';
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <GlobalStyles />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
